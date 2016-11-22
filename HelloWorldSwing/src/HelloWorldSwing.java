f25ec4ebb3b3c4691ccc4e1f24ec4223983b060c
Courtney Glosser <courtneyglosser@gmail.com>
2016-11-22 12:49:29 -0500

    Laying groundwork for more complicated project

---
+++ HelloWorldSwing/src/HelloWorldSwing.java
@@ -0,0 +1,12 @@
+
+/**
+    Simple Hello World application
+ */
+
+public class HelloWorldSwing {
+
+    public static void main (String args[]) {
+        System.out.println("Hello World Swing!");
+    }
+
+}
