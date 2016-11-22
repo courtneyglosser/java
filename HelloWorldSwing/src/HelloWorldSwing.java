3309d370a742f0bd73f927225c3501560e635250
Courtney Glosser <courtneyglosser@gmail.com>
2016-11-22 13:06:02 -0500

    Adding basic Swing JFrame and JPanel architecture

--- HelloWorldSwing/src/HelloWorldSwing.java
+++ HelloWorldSwing/src/HelloWorldSwing.java
@@ -1,12 +1,34 @@
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
 /**
     Simple Hello World application
  */
 
-public class HelloWorldSwing {
+public class HelloWorldSwing extends JFrame {
+
+    public HelloWorldSwing() {
+        System.out.println("Hello World Swing!");
+
+        add(new MyPanel());
+
+        setSize(350, 200);
+        setTitle("Hello World, Swing!");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 
     public static void main (String args[]) {
-        System.out.println("Hello World Swing!");
+        EventQueue.invokeLater( new Runnable() {
+            @Override
+            public void run() {
+                HelloWorldSwing hws = new HelloWorldSwing();
+                hws.setVisible(true);
+            }
+        });
     }
 
 }
---
+++ HelloWorldSwing/src/MyPanel.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel{
+
+    public MyPanel () {
+        System.out.println("Constructing MyPanel");
+    }
+
+}
