2ae888bde34f6ef50317d1630be5f3dc3010f3cf
Courtney Glosser <courtneyglosser@gmail.com>
2017-05-09 17:15:28 -0400

    Simplified with JButton, and refactored stats tracking to dedicated class

--- ScenesRPG/src/ScenesRPGPanel.java
+++ ScenesRPG/src/ScenesRPGPanel.java
@@ -2,6 +2,7 @@
 package cglosser;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
@@ -14,7 +15,6 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.text.DecimalFormat;
 
 /**
     Extendings the Swing JPanel class with Fill game specific settings.  Also,
@@ -33,31 +33,8 @@ public class ScenesRPGPanel extends JPanel implements ActionListener, Runnable{
     private String gameState; // welcome, active, win, lose
     private ButtonManager bm;
     private Screen gameScreen;
+    private Stats gameStats;
 
-    // record stats every 1 second (roughly)
-    private static int MAX_STATS_INTERVAL = 1000;
-    // number of FPS values stored to get an average
-    private static int NUM_FPS = 10;
-
-    // used for gathering statistics
-    private int statsInterval = 0;    // in ms
-    private long prevStatsTime = 0;
-    private int totalElapsedTime = 0;
-    private int gameStartTime;
-    private long timeSpentInGame = 0;    // in seconds
-
-    private int frameCount = 0;
-    private double fpsStore[];
-    private int statsCount = 0;
-    private double averageFPS = 0.0;
-
-    private int framesSkipped = 0;
-    private int totalFramesSkipped = 0;
-    private double upsStore[];
-    private double averageUPS = 0.0;
-
-    private DecimalFormat df = new DecimalFormat("0.##");  // 2 dp
-    private DecimalFormat timedf = new DecimalFormat("0.####");  // 4 dp
 
     private int count;
     private int seconds;
@@ -69,8 +46,6 @@ public class ScenesRPGPanel extends JPanel implements ActionListener, Runnable{
     private volatile boolean gameOver = false;
     private volatile boolean isPaused = false;
 
-    private Font font;
-    private FontMetrics metrics;
     private int score = 0;
 
     private Graphics dbg;
@@ -81,6 +56,53 @@ public class ScenesRPGPanel extends JPanel implements ActionListener, Runnable{
         gameState = "welcome";
         bm = new ButtonManager();
         gameScreen = new Screen();
+        gameStats = new Stats();
+
+        JButton myBtn = new JButton("My Button");
+        myBtn.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Action: " + e);
+            }
+        });
+        this.add(myBtn);
+
+        JButton startBtn = new JButton ("Start");
+        startBtn.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Action: " + e);
+
+                gameState = "active";
+            }
+        });
+        this.add(startBtn);
+
+        JButton exitBtn = new JButton ("Exit");
+        exitBtn.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Action: " + e);
+
+                running = false;
+            }
+        });
+        this.add(exitBtn);
+
+        JButton loseBtn = new JButton ("Lose");
+        loseBtn.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Lose: " + e);
+                gameState = "lose";
+            }
+        });
+        this.add(loseBtn);
+
+        JButton winBtn = new JButton ("Win");
+        winBtn.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Win: " + e);
+                gameState = "win";
+            }
+        });
+        this.add(winBtn);
 
         addMouseListener(new MAdapter());
 
@@ -92,14 +114,6 @@ public class ScenesRPGPanel extends JPanel implements ActionListener, Runnable{
         repaint();
 
 
-        // Timing elements
-        fpsStore = new double[NUM_FPS];
-        upsStore = new double[NUM_FPS];
-
-        for (int i=0; i < NUM_FPS; i++) {
-            fpsStore[i] = 0.0;
-            upsStore[i] = 0.0;
-        }
     }
 
     public void run() {
@@ -129,10 +143,11 @@ public class ScenesRPGPanel extends JPanel implements ActionListener, Runnable{
 
             beforeTime = System.currentTimeMillis();
 
-            storeStats();
+            gameStats.storeStats();
+            requestFocus();
         }
 
-        printStats();
+        gameStats.printStats();
 
         System.exit(0);
     }
@@ -239,7 +254,7 @@ public class ScenesRPGPanel extends JPanel implements ActionListener, Runnable{
                 repaint();
             }
             if (bm.checkExit(gameState, e.getX(), e.getY())) {
-                System.exit(0);
+                running = false;
             }
         }
 
@@ -266,102 +281,6 @@ public class ScenesRPGPanel extends JPanel implements ActionListener, Runnable{
 
 
 
-    /**
-        The statistics:
-        - the summed periods for all the iterations in this interval
-        (period is the amount of time a single frame iteration should take),
-        the actual elapsed time in this interval,
-        the error between these two numbers;
 
-        - the total frame count, which is the total number of calls to run();
-
-        - the frames skipped in this interval, the total number of frames
-        skipped. A frame skip is a game update without a corresponding render;
-
-        - the FPS (frames/sec) and UPS (updates/sec) for this interval,
-        the average FPS & UPS over the last NUM_FPSs intervals.
-
-        The data is collected every MAX_STATS_INTERVAL  (1 sec).
-     */
-    private void storeStats()
-    {
-        frameCount++;
-        statsInterval += period;
-
-        if (prevStatsTime == 0) {
-            prevStatsTime = System.currentTimeMillis();
-        }
-
-        // record stats every MAX_STATS_INTERVAL
-        if (statsInterval >= MAX_STATS_INTERVAL) {
-            long timeNow = System.currentTimeMillis();
-            timeSpentInGame = (timeNow - gameStartTime);
-
-            // time since last stats collection
-            long realElapsedTime = timeNow - prevStatsTime;
-            totalElapsedTime += realElapsedTime / 1000;
-
-            double timingError =
-            ((double)(realElapsedTime - statsInterval) / statsInterval) * 100.0;
-
-            totalFramesSkipped += framesSkipped;
-
-            // calculate the latest FPS and UPS
-            double actualFPS = 0;
-            double actualUPS = 0;
-            if (totalElapsedTime > 0) {
-                actualFPS = ((double)frameCount / totalElapsedTime);
-                actualUPS = ((double)(frameCount + totalFramesSkipped) / totalElapsedTime);
-            }
-
-            // store the latest FPS and UPS
-            fpsStore[ (int)statsCount%NUM_FPS ] = actualFPS;
-            upsStore[ (int)statsCount%NUM_FPS ] = actualUPS;
-            statsCount++;
-
-            // total the stored FPSs and UPSs
-            double totalFPS = 0.0;
-            double totalUPS = 0.0;
-
-            for (int i=0; i < NUM_FPS; i++) {
-                totalFPS += fpsStore[i];
-                totalUPS += upsStore[i];
-            }
-
-            // obtain the average FPS and UPS
-            if (statsCount < NUM_FPS) {
-                averageFPS = totalFPS/statsCount;
-                averageUPS = totalUPS/statsCount;
-            }
-            else {
-                averageFPS = totalFPS/NUM_FPS;
-                averageUPS = totalUPS/NUM_FPS;
-            }
-
-            /**/
-            System.out.println(timedf.format( (double) statsInterval/1000) + " " +
-            timedf.format((double) realElapsedTime/1000) + "s " +
-            df.format(timingError) + "% " +
-            frameCount + "c " +
-            framesSkipped + "/" + totalFramesSkipped + " skip; " +
-            df.format(actualFPS) + " " + df.format(averageFPS) + " afps; " +
-            df.format(actualUPS) + " " + df.format(averageUPS) + " aups" );
-            /**/
-
-
-            framesSkipped = 0;
-            prevStatsTime = timeNow;
-            statsInterval = 0;   // reset
-        }
-    }  // end of storeStats()
-
-
-    private void printStats()
-    {
-        System.out.println("Frame Count/Loss: " + frameCount + " / " + totalFramesSkipped);
-        System.out.println("Average FPS: " + df.format(averageFPS));
-        System.out.println("Average UPS: " + df.format(averageUPS));
-        System.out.println("Time Spent: " + timeSpentInGame + " secs");
-    }  // end of printStats()
 
 }
---
+++ ScenesRPG/src/Stats.java
@@ -0,0 +1,158 @@
+
+package cglosser;
+
+
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.text.DecimalFormat;
+
+
+public class Stats {
+    // record stats every 1 second (roughly)
+    private static int MAX_STATS_INTERVAL = 1000;
+    // number of FPS values stored to get an average
+    private static int NUM_FPS = 10;
+
+
+    private int statsInterval = 0;    // in ms
+    private long prevStatsTime = 0;
+
+    private int gameStartTime;
+    private long timeSpentInGame = 0;    // in seconds
+    private int totalElapsedTime = 0;
+
+    private int framesSkipped = 0;
+    private int totalFramesSkipped = 0;
+    private int period = 1000/100; // ms / FPS;
+
+    private double fpsStore[];
+    private int statsCount = 0;
+    private double averageFPS = 0.0;
+
+    private double upsStore[];
+    private double averageUPS = 0.0;
+
+    private int frameCount = 0;
+
+
+    private DecimalFormat df = new DecimalFormat("0.##");  // 2 dp
+    private DecimalFormat timedf = new DecimalFormat("0.####");  // 4 dp
+
+    private Font font;
+    private FontMetrics metrics;
+
+    public Stats() {
+        System.out.println("In Stats constructor");
+        // Timing elements
+        fpsStore = new double[NUM_FPS];
+        upsStore = new double[NUM_FPS];
+
+        System.out.println("Not Initialized fpsStore: " + fpsStore);
+
+        for (int i=0; i < NUM_FPS; i++) {
+            fpsStore[i] = 0.0;
+            upsStore[i] = 0.0;
+        }
+
+
+        System.out.println("Initialized fpsStore: " + fpsStore);
+    }
+
+    /**
+        The statistics:
+        - the summed periods for all the iterations in this interval
+        (period is the amount of time a single frame iteration should take),
+        the actual elapsed time in this interval,
+        the error between these two numbers;
+
+        - the total frame count, which is the total number of calls to run();
+
+        - the frames skipped in this interval, the total number of frames
+        skipped. A frame skip is a game update without a corresponding render;
+
+        - the FPS (frames/sec) and UPS (updates/sec) for this interval,
+        the average FPS & UPS over the last NUM_FPSs intervals.
+
+        The data is collected every MAX_STATS_INTERVAL  (1 sec).
+     */
+    public void storeStats()
+    {
+        frameCount++;
+        statsInterval += period;
+
+        if (prevStatsTime == 0) {
+            prevStatsTime = System.currentTimeMillis();
+        }
+
+        // record stats every MAX_STATS_INTERVAL
+        if (statsInterval >= MAX_STATS_INTERVAL) {
+            long timeNow = System.currentTimeMillis();
+            timeSpentInGame = (timeNow - gameStartTime);
+
+            // time since last stats collection
+            long realElapsedTime = timeNow - prevStatsTime;
+            totalElapsedTime += realElapsedTime / 1000;
+
+            double timingError =
+            ((double)(realElapsedTime - statsInterval) / statsInterval) * 100.0;
+
+            totalFramesSkipped += framesSkipped;
+
+            // calculate the latest FPS and UPS
+            double actualFPS = 0;
+            double actualUPS = 0;
+            if (totalElapsedTime > 0) {
+                actualFPS = ((double)frameCount / totalElapsedTime);
+                actualUPS = ((double)(frameCount + totalFramesSkipped) / totalElapsedTime);
+            }
+
+            // store the latest FPS and UPS
+            fpsStore[ (int)statsCount%NUM_FPS ] = actualFPS;
+            upsStore[ (int)statsCount%NUM_FPS ] = actualUPS;
+            statsCount++;
+
+            // total the stored FPSs and UPSs
+            double totalFPS = 0.0;
+            double totalUPS = 0.0;
+
+            for (int i=0; i < NUM_FPS; i++) {
+                totalFPS += fpsStore[i];
+                totalUPS += upsStore[i];
+            }
+
+            // obtain the average FPS and UPS
+            if (statsCount < NUM_FPS) {
+                averageFPS = totalFPS/statsCount;
+                averageUPS = totalUPS/statsCount;
+            }
+            else {
+                averageFPS = totalFPS/NUM_FPS;
+                averageUPS = totalUPS/NUM_FPS;
+            }
+
+            /**/
+            System.out.println(timedf.format( (double) statsInterval/1000) + " " +
+            timedf.format((double) realElapsedTime/1000) + "s " +
+            df.format(timingError) + "% " +
+            frameCount + "c " +
+            framesSkipped + "/" + totalFramesSkipped + " skip; " +
+            df.format(actualFPS) + " " + df.format(averageFPS) + " afps; " +
+            df.format(actualUPS) + " " + df.format(averageUPS) + " aups" );
+            /**/
+
+
+            framesSkipped = 0;
+            prevStatsTime = timeNow;
+            statsInterval = 0;   // reset
+        }
+    }  // end of storeStats()
+
+    public void printStats()
+    {
+        System.out.println("Frame Count/Loss: " + frameCount + " / " + totalFramesSkipped);
+        System.out.println("Average FPS: " + df.format(averageFPS));
+        System.out.println("Average UPS: " + df.format(averageUPS));
+        System.out.println("Time Spent: " + timeSpentInGame + " secs");
+    }  // end of printStats()
+
+}
